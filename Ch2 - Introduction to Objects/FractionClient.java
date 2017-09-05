32e020498d8e7a82ad2ee49edd8f16ebcff6d4b9
Dmitryi <dmwe@telenor.dk>
2017-09-05 23:29:58 +0300

    Removed unnecessary code in FractionClient

--- Ch2 - Introduction to Objects/FractionClient.java
+++ Ch2 - Introduction to Objects/FractionClient.java
@@ -1,4 +1,4 @@
-public class FractionClient implements Cloneable{
+public class FractionClient{
 
     public static void main(String[] args)  {
         Fraction fraction1 = new Fraction(-1, 2);
@@ -11,7 +11,7 @@ public class FractionClient implements Cloneable{
         System.out.println();
 
         System.out.println("The sum of (fraction1 + fraction2): " + fraction1.add(fraction2));
-        System.out.println("The substraction of (fraction1 - fraction2): " + fraction1.subtract(fraction2));
+        System.out.println("The subtraction of (fraction1 - fraction2): " + fraction1.subtract(fraction2));
         System.out.println("The multiplication of (fraction1 * fraction2): " + fraction1.multiply(fraction2));
         System.out.println("The division of (fraction1 / fraction2: " + fraction1.divide(fraction2));
     }
